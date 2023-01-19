61ce60365eb97ce2852b0470e360cd460803f3a8
Olexandr Kovalenko <22bendera.s@gmail.com>
2023-01-19 12:12:23 +0200

    add superpower to Hero, getSuperheroWithPower

--- controllers/superhero.controller.js
+++ controllers/superhero.controller.js
@@ -1,4 +1,4 @@
-const {Superhero} = require('../models');
+const {Superhero, Superpower, Image} = require('../models');
 
 module.exports.createSuperhero = async (req, res, next) => {
     try {
@@ -12,8 +12,22 @@ module.exports.createSuperhero = async (req, res, next) => {
 
 module.exports.getOneSuperhero = async (req, res, next) => {
     try {
-        const {superheroInstance} = req;
-        res.status(200).send(superheroInstance);
+        // const {superheroInstance} = req;
+        // const superpowers = await superheroInstance.getSuperpowers();
+        // res.status(200).send({superheroInstance, superpowers});
+        const {params: {heroId}} = req;
+        const hero = await Superhero.findByPk(heroId, {
+            include: [{
+                model: Superpower,
+                attributes: {
+                    exclude: ['createdAt', 'updatedAt', 'id']
+                }
+            }]
+        });
+        if(!hero) {
+            throw new NotFoundError('Superhero not found');
+        }
+        res.status(200).send(hero);
     } catch (error) {
         next(error);
     }
@@ -33,13 +47,28 @@ module.exports.getAllSuperheroes = async (req, res, next) => {
 
 module.exports.updateSuperhero = async (req, res, next) => {
     try {
-        const {params: {heroId}, body: {nickname, realName, catchPhrase, originDescription}} = req;
+        const {params: {heroId}, body: {nickname, realName, catchPhrase, originDescription, superpower}} = req;
         const [rowCount, [updatedHero]] = await Superhero.update({nickname, realName, catchPhrase, originDescription}, {
             where: {
                 id: heroId
             },
             returning: true
         });
+        await superpower.map(async power => {
+            const findedPower = await Superpower.findOne({
+                where: {
+                    superpower: power
+                }
+            });
+            if(findedPower === null){
+                const createdSuperpower = await Superpower.create({superpower: power});
+                const addPowerToHero = await updatedHero.addSuperpower(createdSuperpower);
+            } else {
+                if(!await updatedHero.hasSuperpower(findedPower)) {
+                    await updatedHero.addSuperpower(findedPower);
+                }
+            }
+        })
         res.status(200).send(updatedHero);
     } catch (error) {
         next(error);
